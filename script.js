8dd553243740a7cb9aa0e644e9fb2df6a0339520
fhgjnkf <aagya.shrestha2@gmail.com>
2023-05-22 21:07:27 +0545

    my store

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraph = document.createElement("p");
+    paragraph.textContent = "OUT OF STOCK :)";
+    paragraph.classList.add("js-paragraph");
+    document.body.appendChild(paragraph);
+  });
+  
